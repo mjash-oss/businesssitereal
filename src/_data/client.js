3ca70d9be44045369b4291c412eacbe7ce27dd65
mjash-oss <mj.ashleymartin@gmail.com>
2025-06-18 21:11:30 -0500

    first commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,17 +1,8 @@
 module.exports = {
     name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
-    address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
-    },
+    email: "ashleymartinmaya@gmail.com",
+    phoneForTel: "314-203-0159",
+    phoneFormatted: "(314) 203-0159",
     socials: {
         facebook: "https://www.facebook.com/",
         instagram: "https://www.instagram.com/",
